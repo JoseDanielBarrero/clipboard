b40f79cf28dc21fec7aaef00c9555ed588171620
Jose Daniel Barrero Barrios <jdbarrerob@gmail.com>
2024-01-05 20:21:41 -0600

    Added the possible rotations (not included summer rotation) and a possible render

--- src/Components/Clipboard/Clipboard.jsx
+++ src/Components/Clipboard/Clipboard.jsx
@@ -8,6 +8,13 @@ function Clipboard(props) {
   const oasisSpots = ["River 6", "Wave 1", "Wave 2", "B. Twister", "B. Rhino"];
   const mirageSpots = ["B. Victoria", "B. Master", "Leopard R", "Leopard T", "River 5", "River 4","River 3"];
   const islandSpots = ["Tikos Roamer", "Tikos River", "Activity 1", "Activity 2"];
+  const oldTowerSpots = ["T. Twister", "T. ZigZag", "T. Rhino", "T. Elephant"];
+  const newTowerSpots = ["T. Master B", "T. Victoria", "Conveyor"];
+  const flowRiderSpots = ["Flow 1", "Flow 2", "Flow 3"];
+  const DropSpots = ["T. Drop"];
+  const TowelSpots = ["Towels"];
+  const supSpots = ["Old Side" ,  "New Side"];
+  const frontDeskSpots = ["Front Desk"];
 
   return (
     <section className={props.className} >
@@ -15,6 +22,16 @@ function Clipboard(props) {
     <Rotation name={"Oasis"} spots={oasisSpots}/>
     <Rotation name={"Mirage"} spots={mirageSpots}/>
     <Rotation name={"Island"} spots={islandSpots}/>
+    <Rotation name={"Old Tower"} spots={oldTowerSpots}/>
+    <Rotation name={"New Tower"} spots={newTowerSpots}/>
+    <Rotation name={"Flow Rider"} spots={flowRiderSpots}/>
+    {/* <div className='small-rotation'> */}
+      <Rotation name={"Drop Tower"} spots={DropSpots}/>
+      <Rotation name={"Towels"} spots={TowelSpots}/>
+      <Rotation name={"Supervisors"} spots={supSpots}/>
+      <Rotation name={"Front Desk"} spots={frontDeskSpots}/>
+    {/* </div> */}
+    
     {/* <div>
       <div className='prueba'>
       <h1>Doble</h1>
